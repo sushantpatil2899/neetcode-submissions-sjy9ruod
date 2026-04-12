7e5898085bb3bd710b4df3552aabbf67005fb933
Sushant Patil <sushant.patil@stonybrook.edu>
2026-04-12 12:37:11 -0400

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int l = 0, r = s.length() -1, c = 1;
+
+        while (l < r) {
+            if (s[l] != s[r]) {
+                return isPalindrome(s, l+1, r) ||
+                    isPalindrome(s, l, r-1);
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+private:
+    bool isPalindrome(const string& s, int l, int r) {
+        while (l < r) {
+            if (s[l++] != s[r--]) return false;
+        }
+        return true;
+    }
+};
