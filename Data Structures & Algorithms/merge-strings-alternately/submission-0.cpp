29f4a7997c3e2a25359040fab8d1a927e0d37c9b
Sushant Patil <sushant.patil@stonybrook.edu>
2026-04-14 13:03:59 -0400

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i = 0, j = 0;
+        string s = "";
+        while (i < word1.length() && j < word2.length()) {
+            s += word1[i++];
+            s += word2[j++];
+        }
+
+        if (i != word1.length()) s += word1.substr(i);
+        if (j != word2.length()) s += word2.substr(j);
+        return s;
+    }
+};
