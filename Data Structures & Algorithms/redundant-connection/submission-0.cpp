982a7ed074c18e962821f170bbe5ecda88921ccd
Sushant Patil <sushant.patil@stonybrook.edu>
2026-05-27 11:23:09 -0400

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    vector<bool> visit;
+    vector<vector<int>> adj;
+    unordered_set<int> cycle;
+    int cycleStart;
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        adj.resize(n + 1);
+        for (auto& edge: edges) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+
+        visit.resize(n + 1, false);
+        cycleStart = -1;
+        dfs(1, -1);
+
+        for (int i = edges.size() - 1; i >= 0; i--) {
+            int u = edges[i][0], v = edges[i][1];
+            if (cycle.count(u) && cycle.count(v)) return {u, v};
+        }
+
+        return {};
+    }
+
+    bool dfs(int node, int par) {
+        if (visit[node]) {
+            cycleStart = node;
+            return true;
+        }
+        visit[node] = true;
+        for (int nei: adj[node]) {
+            if (nei == par) continue;
+            if (dfs(nei, node)) {
+                if (cycleStart != -1) cycle.insert(node);
+                if (node == cycleStart) {
+                    cycleStart = -1;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+};
